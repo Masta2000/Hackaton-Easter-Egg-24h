e6f12f4198c25c4d7ae7897046de1e22961d6e48
Spliit <mcuville@gmail.com>
2019-04-19 00:29:32 +0200

    fix btn

--- src/Search/Filter.jsx
+++ src/Search/Filter.jsx
@@ -1,71 +1,79 @@
-import React, { Component } from 'react';
-import './Filter.css'
+import React, { Component } from "react";
+import "./Filter.css";
 class Filter extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: "",
-            displayList: [],
-            character: [
-                {"image":"Images/Adjudicator_Rick.png",},
-                {"image":"Images/Albert_Einstein.png",},
-                {"image":"Images/Alien_Rick.png",},
-                {"image":"Images/Beth_Smith.png",},
-                {"image":"Images/Cult_Leader_Morty.png",},
-                {"image":"Images/Jerry_Smith.png",},
-                {"image":"Images/Morty_Smith_.png",},
-                {"image":"Images/Rick_Sanchez.png",},
-                {"image":"Images/Summer_Smith.png",},
-            ],
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      displayList: [],
+      character: [
+        { image: "Images/Adjudicator_Rick.png" },
+        { image: "Images/Albert_Einstein.png" },
+        { image: "Images/Alien_Rick.png" },
+        { image: "Images/Beth_Smith.png" },
+        { image: "Images/Cult_Leader_Morty.png" },
+        { image: "Images/Jerry_Smith.png" },
+        { image: "Images/Morty_Smith_.png" },
+        { image: "Images/Rick_Sanchez.png" },
+        { image: "Images/Summer_Smith.png" }
+      ],
 
-            egg: [
-                {"image":"Images/Egg_of_chameleon.png",},
-                {"image":"Images/Egg_of_fighting_Fish_Siamese.png",},
-                {"image":"Images/Egg_of_Fighting.png",},
-                {"image":"Images/Egg_of_Frog_Bull.png",},
-                {"image":"Images/Egg_of_Gull_Heiring.png",},
-                {"image":"Images/Egg_of_Hellbender copie.png",},
-                {"image":"Images/Egg_of_Ostrich copie.png",},
-                {"image":"Images/Egg_of_Sturgeon_Atlantic.png",},
-                {"image":"Images/Strange_Egg copie.png",},
-            ]
-        }
-    }
+      egg: [
+        { image: "Images/Egg_of_chameleon.png" },
+        { image: "Images/Egg_of_fighting_Fish_Siamese.png" },
+        { image: "Images/Egg_of_Fighting.png" },
+        { image: "Images/Egg_of_Frog_Bull.png" },
+        { image: "Images/Egg_of_Gull_Heiring.png" },
+        { image: "Images/Egg_of_Hellbender copie.png" },
+        { image: "Images/Egg_of_Ostrich copie.png" },
+        { image: "Images/Egg_of_Sturgeon_Atlantic.png" },
+        { image: "Images/Strange_Egg copie.png" }
+      ]
+    };
+  }
 
-    handleClickCharacter = () => {
-        this.setState({
-            displayList: [...this.state.character]
-        })
-    }
+  handleClickCharacter = () => {
+    this.setState({
+      displayList: [...this.state.character]
+    });
+  };
 
-    handleClickEgg = () => {
-        this.setState({
-            displayList: [...this.state.egg]
-        })
-    }
+  handleClickEgg = () => {
+    this.setState({
+      displayList: [...this.state.egg]
+    });
+  };
 
-    handleClickAll = event => {
-        this.setState({
-            displayList: [...this.state.egg, ...this.state.character]
-        })
-    }
+  handleClickAll = event => {
+    this.setState({
+      displayList: [...this.state.egg, ...this.state.character]
+    });
+  };
 
-    render () {
-        return (
-            <div className="Filter">
-                <button className="button" onClick={this.handleClickEgg}>Egg</button>
-                <button  className="button" onClick={this.handleClickCharacter}>Characters</button>
-                <button className="button" onClick={this.handleClickAll}>All</button>
-                <ul>
-                    {
-                        this.state.displayList.map(el => <li className="card col-xs-12 col-sm-6 col-lg-3 row justify-content-center">{el.color} <img src={el.image}/></li>)
-                    }
-                </ul>
-            
-            </div>
-            
-        )
-    }
- }
+  render() {
+    return (
+      <div className="Filter container-fluid">
+        <div className="row justify-content-center">
+            <button className="button btn" onClick={this.handleClickEgg}>
+              Egg
+            </button>
+            <button className="button btn" onClick={this.handleClickCharacter}>
+              Characters
+            </button>
+            <button className="button btn" onClick={this.handleClickAll}>
+              All
+            </button>
+        </div>
+        <ul className="cards row justify-content-center">
+          {this.state.displayList.map(el => (
+            <li className="card col-xs-12 col-sm-6 col-lg-3 row justify-content-center">
+              {el.color} <img src={el.image} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default Filter;
