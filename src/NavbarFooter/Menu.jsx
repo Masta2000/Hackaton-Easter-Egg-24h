ccbfb729043a2e5ceeec38f2ba3467b3eaf6314a
Guillaume Loup <guillaume.loup@live.fr>
2019-04-19 09:04:13 +0200

    modification navbar

--- src/NavbarFooter/Menu.jsx
+++ src/NavbarFooter/Menu.jsx
@@ -1,46 +1,50 @@
 import React, { Component } from 'react';
 
 class Menu extends Component {
-    constructor(props){
-      super(props);
-      this.state={
-        open: this.props.open? this.props.open:false,
-      }
-    }
-      
-    componentWillReceiveProps(nextProps){
-      if(nextProps.open !== this.state.open){
-        this.setState({open:nextProps.open});
-      }
-    }
-    
-    render(){
-      const styles={
-        container: {
-          position: 'relative',
-          top: 0,
-          left: 0,
-          height: this.state.open? '25%': 0,
-          display: 'flex',
-          flexDirection: 'column',
-          background: '#FEFCFB',
-          opacity: 0.95,
-          color: '#000000',
-          transition: 'height 0.3s ease',
-          zIndex: 2,
-        },
-      }
-      return(
-        <div style={styles.container} className="menu">
-          {
-            this.state.open?
-              <div style={styles.menuList}>
-                {this.props.children}
-              </div>:null
-          }
-        </div>
-      )
-    }
-  }
-  
+   constructor(props){
+     super(props);
+     this.state={
+       open: this.props.open? this.props.open:false,
+     }
+   }
+
+   componentWillReceiveProps(nextProps){
+     if(nextProps.open !== this.state.open){
+       this.setState({open:nextProps.open});
+     }
+   }
+
+   render(){
+     const styles={
+       container: {
+         position: 'absolute',
+         top: 0,
+         left: 0,
+         height: this.state.open? '25%': 0,
+         width: '100vw',
+         display: 'flex',
+         flexDirection: 'column',
+         background: '#FEFCFB',
+         opacity: 0.95,
+         color: '#000000',
+         transition: 'height 0.3s ease',
+         zIndex: 2,
+       },
+       menuList: {
+         paddingTop: '3rem',
+       }
+     }
+     return(
+       <div style={styles.container}>
+         {
+           this.state.open?
+             <div style={styles.menuList}>
+               {this.props.children}
+             </div>:null
+         }
+       </div>
+     )
+   }
+ }
+
 export default Menu
--- src/NavbarFooter/Navbar.jsx
+++ src/NavbarFooter/Navbar.jsx
@@ -1,86 +1,80 @@
-import React, { Component } from "react";
-import MenuItem from "./MenuItem";
-import Menu from "./Menu";
-import MenuButton from "./MenuButton";
+import React, { Component } from 'react'
+import MenuItem from './MenuItem'
+import Menu from './Menu'
+import MenuButton from './MenuButton'
 
-import "./Navbar.css";
+import './Navbar.css';
 
 class Navbar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      menuOpen: false
-    };
-  }
+   constructor(props){
+     super(props);
+     this.state={
+       menuOpen:false,
+     }
+   }
 
-  handleMenuClick() {
-    this.setState({ menuOpen: !this.state.menuOpen });
-  }
+   handleMenuClick() {
+     this.setState({menuOpen:!this.state.menuOpen});
+   }
 
-  handleLinkClick() {
-    this.setState({ menuOpen: false });
-  }
+   handleLinkClick() {
+     this.setState({menuOpen: false});
+   }
 
-  render() {
-    const styles = {
-      container: {
-        position: "relative",
-        top: 0,
-        left: 0,
-        zIndex: "99",
-        opacity: 0.9,
-        display: "flex",
-        alignItems: "center",
-        background: "#FEFCFB",
-        width: "100%",
-        color: "#000000",
-        fontFamily: "Lobster"
-      },
-      logo: {
-        margin: "0 auto"
-      },
-      body: {
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        filter: this.state.menuOpen ? "blur(2px)" : null,
-        transition: "filter 0.5s ease"
-      }
-    };
-    const menu = ["Home", "Our Collection"];
-    const menuItems = menu.map((val, index) => {
-      return (
-        <MenuItem
-          key={index}
-          delay={`${index * 0.1}s`}
-          onClick={() => {
-            this.handleLinkClick();
-          }}
-        >
-          {val}
-        </MenuItem>
-      );
-    });
+   render(){
+     const styles=
+       {
+         container:{
+           position: 'absolute',
+           top: 0,
+           left: 0,
+           zIndex: '99',
+           opacity: 0.9,
+           display:'flex',
+           alignItems:'center',
+           background: '#FEFCFB',
+           width: '100%',
+           color: '#000000',
+           fontFamily:'Lobster',
+         },
+         logo: {
+           margin: '0 auto',
+         },
+         body: {
+           display: 'flex',
+           flexDirection: 'column',
+           alignItems: 'center',
+           filter: this.state.menuOpen ? 'blur(2px)':null,
+           transition: 'filter 0.5s ease',
+         },
+       }
+     const menu = ['Home','Our Collection']
+     const menuItems = menu.map((val,index)=>{
+       return (
+         <MenuItem
+           key={index}
+           delay={`${index * 0.1}s`}
+           onClick={()=>{this.handleLinkClick();}}>{val}</MenuItem>)
+     });
 
-    return (
-      <div className="Navbar">
-        <div style={styles.container}>
-          <MenuButton
-            className="menubtn"
-            onClick={() => this.handleMenuClick()}
-            color="white"
-          />
-          <img src="images/yoshi orange.png" className="Logo" alt="logo" />
-          <img src="images/yoshi green.png" className="Logo" alt="logo" />
-          <img src="images/Yoshi Red.png" className="Logo" alt="logo" />
-          <h1 className="Homepage">Easter and Co.</h1>
-        </div>
+     return(
+       <div>
+         <div style={styles.container}>
+           <MenuButton open={this.state.menuOpen} onClick={()=>this.handleMenuClick()} color='white'/>
+           <img src='images/yoshi orange.png' className="Logo" alt ="logo"/>
+           <img src='images/yoshi green.png' className="Logo" alt ="logo"/>
+           <img src='images/Yoshi Red.png' className="Logo" alt ="logo"/>
+           <h1 className="Homepage">Easter and Co.</h1>
+         </div>
 
-        <Menu open={this.state.menuOpen}>{menuItems}</Menu>
-        <div style={styles.body} />
-      </div>
-    );
-  }
-}
+         <Menu open={this.state.menuOpen}>
+           {menuItems}
+         </Menu>
+         <div style={styles.body}>
+         </div>
+       </div>
+     )
+   }
+ }
 
-export default Navbar;
+ export default Navbar
